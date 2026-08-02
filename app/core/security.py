ef4396bef43a288747299c5f6f0b8c2e094ab678
Hussnain Saeed <hussnainkhanai5@gmail.com>
2026-08-02 17:17:00 +0500

    make changings in security.py file

--- app/core/security.py
+++ app/core/security.py
@@ -3,6 +3,10 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 import jwt
+from fastapi import Depends,HTTPException
+from fastapi.security import OAuth2PasswordBearer
+outh_scheme=OAuth2PasswordBearer(tokenUrl="/auth/login")
+
 
 load_dotenv()
 
@@ -25,6 +29,17 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(token: str = Depends(outh_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_id
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
 
 
 
